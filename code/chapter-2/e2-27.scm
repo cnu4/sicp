ddd18d26d4b2310f65b54560cc0351ea9e436305
cnu4 <fangxw1004@qq.com>
2018-07-11 23:56:14 +0800

    e2-27 e2-28

---
+++ code/chapter-2/e2-27.scm
@@ -0,0 +1,8 @@
+(define (deep-reverse tree)
+  (cond ((null? tree) '())
+    ((not (pair? tree)) tree)
+    (else (reverse (list (deep-reverse (car tree))
+                         (deep-reverse (cadr tree)))))))
+
+(define x (list (list 1 2) (list 3 4)))
+(deep-reverse x)
---
+++ code/chapter-2/e2-28.scm
@@ -0,0 +1,8 @@
+(define (fringe tree)
+  (cond ((null? tree) '())
+    ((not (pair? tree)) (list tree))
+    (else (append (fringe (car tree))
+                  (fringe (cadr tree))))))
+
+(define x (list (list 1 2) (list 3 4)))
+(fringe x)
