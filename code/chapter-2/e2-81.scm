7f0f00f61af6748da4549ab874fb86a5ef60147b
cnu4 <fangxw1004@qq.com>
2018-10-20 10:18:04 +0800

    add e3-21 22

--- code/chapter-2/e2-81.scm
+++ code/chapter-2/e2-81.scm
@@ -11,9 +11,9 @@
                     (let ((type1 (car type-tags))
                           (type2 (cadr type-tags))
                           (a1 (car args))
-                          (a2 (cadr args)))
+                          (a2 (cadr args)))   
                         (if (equal? type1 type2) ; is equal
-                            (error "No method for these types" (list op type-tags)) ; 
+                            (error "No method for these types" (list op type-tags)) ;
                             (let ((t1->t2 (get-coercion type1 type2))
                                   (t2->t1 (get-coercion type2 type1)))
                                 (cond (t1->t2
